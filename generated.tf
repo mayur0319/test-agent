d3de20554ba747cec19dfc957bf997df772edf3f
MayurSabade <79620475+mayur0319@users.noreply.github.com>
2026-06-15 18:30:44 +0530

    generate: Create an S3 bucket with versioning, and block all public ac

---
+++ generated.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+variable "bucket_name_prefix" {
+  default = "mayur-terrafrom-agent-1"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "${var.bucket_name_prefix}-${random_id.suffix.hex}"
+  tags = {
+    Name = "Terraform-managed S3 bucket"
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+      kms_master_key_id = "alias/aws/s3"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.example.bucket
+}
